ea671305a5d0c98d9e1143948eb793474ce6595a
Harsh Mishra <harshmishrawork16@gmail.com>
2026-03-08 16:22:55 +0000

    about js variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const name="harsh"
+let age=20
+var address="1234"
+
+
+age=21
+address="2234"
+let salary;
+
+console.table([age,address,salary]);
+/*
+avoid using var due to the issue of scope
+
+*/
