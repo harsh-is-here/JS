3a78bebb7d7b54aa782706f224db948b17f17a8c
Harsh Mishra <harshmishrawork16@gmail.com>
2026-03-09 08:27:49 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let number=33;
+console.log(typeof number);
+let number2="33"
+let number3=Number(number2)
+console.log(typeof number3);
+let number4="33abc"
+console.log(typeof number4);
+let number5=Number(number4)
+console.log(typeof number5);
+console.log(number5);
+
+//"33"=>33
+//string=>NaN
+//"33abc"=>NaN but type =>Number
+
+let isLoggedIn=""
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//1=>true,o=>false
+//""=>false
+//"harsh"=>ture
+
+let someNumber=33
+let stringSomeNumber=String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
+
+
+
+
